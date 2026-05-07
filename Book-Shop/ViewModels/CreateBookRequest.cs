fcce9440b868fd1467ff1b724151830a67c122ee
MirTaleh <ibrahimovmirtaleh@gmail.com>
2026-05-07 09:16:27 +0400

    'firstCommit'

---
+++ Book-Shop/ViewModels/CreateBookRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Shop.ViewModels
+{
+    public class CreateBookRequest
+    {
+        [Required(ErrorMessage = "Kitabın adı boş ola bilməz")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Müəllif adı lazımdır")]
+        public string Author { get; set; }
+        [Range(1, 1000, ErrorMessage = "Qiymət 1-1000 arasında olmalıdır")]
+        [Compare("Price", ErrorMessage = "Qiymətlər uyğun deyil")]
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
