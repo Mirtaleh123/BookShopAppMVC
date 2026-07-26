17269bca864ba4cd4bfa65f61242e06c950436fa
MirTaleh <ibrahimovmirtaleh@gmail.com>
2026-07-26 13:02:41 +0400

    Remove all files

--- Book-Shop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Book_Shop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
